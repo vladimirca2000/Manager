0ad2e478cf551ba3c082e503e5cf3928d2cbca63
vladimirca2000 <vladimirca2000@gmail.com>
2021-04-24 21:40:42 -0300

    Camada Infra

--- src/Manager.Domain/Entities/Base.cs
+++ src/Manager.Domain/Entities/Base.cs
@@ -1,14 +1,12 @@
 using System.Collections.Generic;
 
-namespace Manager.Domain.Entities
-{
-    public abstract class Base
-    {
-        public Long Id { get; set; }
+namespace Manager.Domain.Entities{
+    public abstract class Base{
+        public long Id { get; set; }
 
-        internal List<string> _erros;
-        public IReadOnlyCollection<string> Erros => _erros;
+        internal List<string> _errors;
+        public IReadOnlyCollection<string> Errors => _errors;
 
         public abstract bool Validate();
-    }
+    } 
 }
--- src/Manager.Domain/Entities/User.cs
+++ src/Manager.Domain/Entities/User.cs
@@ -1,5 +1,5 @@
-using Manager.Core.Exceptions;
 using System.Collections.Generic;
+using Manager.Core.Exceptions;
 using Manager.Domain.Validators;
 
 
@@ -7,13 +7,14 @@ namespace Manager.Domain.Entities
 {
     public class User : Base
     {
+
         //Propriedades
         public string Name { get; private set; }
         public string Email { get; private set; }
         public string Password { get; private set; }
 
         //EF
-        protected User(){}
+        protected User() { }
 
         public User(string name, string email, string password)
         {
@@ -27,17 +28,20 @@ namespace Manager.Domain.Entities
 
 
         //Comportamentos
-        public void ChangeName(string name){
+        public void ChangeName(string name)
+        {
             Name = name;
             Validate();
         }
 
-        public void ChangePassword(string password){
+        public void ChangePassword(string password)
+        {
             Password = password;
             Validate();
         }
 
-        public void ChangeEmail(string email){
+        public void ChangeEmail(string email)
+        {
             Email = email;
             Validate();
         }
@@ -48,8 +52,9 @@ namespace Manager.Domain.Entities
             var validator = new UserValidator();
             var validation = validator.Validate(this);
 
-            if(!validation.IsValid){
-                foreach(var error in validation.Errors)
+            if (!validation.IsValid)
+            {
+                foreach (var error in validation.Errors)
                     _errors.Add(error.ErrorMessage);
 
                 throw new DomainException("Alguns campos estão inválidos, por favor corrija-os!", _errors);
